df657ece3b79fe9b4f5ff90289f2b7be487d75f4
Gabu-Rayon <gibsonmurimi4@gmail.com>
2023-01-26 16:17:04 +0300

    Made change by applying PHP8 OOP

--- classes.php
+++ classes.php
@@ -1,20 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-<?php
-class Recipe{
-    var $image;
-    var $title;
-    var $description;
-    var $ingredients;
-    var $equipments;
-    function __construct($image,$title,$description,$ingredients,$equipments){
+    <?php
+// class Recipe{
+//     var $image;
+//     var $title;
+//     var $description;
+//     var $ingredients;
+//     var $equipments;
+//     function __construct($image,$title,$description,$ingredients,$equipments){
+//         $this->image = $image;
+//         $this->title = $title;
+//         $this->description = $description;
+//         $this->ingredients = $ingredients;
+//         $this->equipments = $equipments;
+//     }
+    // Using PHP8 OOP
+    class Recipe{
+    public string $image;
+    public string $title;
+    public string $description;
+    public string $ingredients;
+    public string $equipments;
+    public function __construct(string $image,string $title,string $description,string $ingredients,string $equipments){
         $this->image = $image;
         $this->title = $title;
         $this->description = $description;
@@ -22,6 +38,8 @@ class Recipe{
         $this->equipments = $equipments;
     }
 }
+
 ?>
 </body>
+
 </html>
